0ef3d7ea1a11aa7c932c6a8d67e39ce6123f0306
Christoffer H. Nielsen <hecn@itu.dk>
2022-12-29 16:22:43 +0100

    Full implementation and logging

--- client/client.go
+++ client/client.go
@@ -18,14 +18,21 @@ import (
 
 // Same principle as in client. Flags allows for user specific arguments/values
 var clientsName = flag.String("name", "default", "Senders name")
-var serverPort = flag.String("server", "5400", "Tcp server")
+var serverPort0 = flag.String("server0", "5000", "Tcp server")
+var serverPort1 = flag.String("server1", "5001", "Tcp server")
+var serverPort2 = flag.String("server2", "5001", "Tcp server")
 
-var server gRPC.TemplateClient  //the server
-var ServerConn *grpc.ClientConn //the server connection
+var servers = []gRPC.TemplateClient{}
+var serverConns []*grpc.ClientConn
+
+var responseNumber = 0
+var currentMessage string
 
 func main() {
 	//parse flag/arguments
 	flag.Parse()
+	f := setLog()
+	defer f.Close()
 
 	fmt.Println("--- CLIENT APP ---")
 
@@ -34,120 +41,70 @@ func main() {
 	//defer f.Close()
 
 	//connect to server and close the connection when program closes
-	fmt.Println("--- join Server ---")
-	ConnectToServer()
-	defer ServerConn.Close()
+	for i := 0; i < 3; i++ {
+		serverName := "server" + strconv.Itoa(i) // name of the server
+		serverPort := "500" + strconv.Itoa(i)    // port of the server port
+		fmt.Println("Servername: " + serverName + " - Serverport: " + serverPort)
+		ConnectToServer(serverName, serverPort)
+	}
 
-	//start the biding
-	parseInput()
+	//start the increment calls
+	ParseInput()
 }
 
 // connect to server
-func ConnectToServer() {
-
-	//dial options
-	//the server is not using TLS, so we use insecure credentials
-	//(should be fine for local testing but not in the real world)
+func ConnectToServer(serverName string, serverPort string) {
+	serverFlag := serverPort
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	//dial the server, with the flag "server", to get a connection to it
-	log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, *serverPort)
-	conn, err := grpc.Dial(fmt.Sprintf(":%s", *serverPort), opts...)
+	fmt.Printf("client %s: Attempts to dial on port %s\n", *clientsName, serverFlag)
+	conn, err := grpc.Dial(fmt.Sprintf(":%s", serverFlag), opts...)
 	if err != nil {
-		log.Printf("Fail to Dial : %v", err)
+		fmt.Printf("Fail to Dial : %v", err)
 		return
 	}
 
-	// makes a client from the server connection and saves the connection
-	// and prints rather or not the connection was is READY
-	server = gRPC.NewTemplateClient(conn)
-	ServerConn = conn
-	log.Println("the connection is: ", conn.GetState().String())
+	servers = append(servers, gRPC.NewTemplateClient(conn))
+	serverConns = append(serverConns, conn)
 }
 
-func parseInput() {
+func ParseInput() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Type the amount you wish to increment with here. Type 0 to get the current value")
+	fmt.Println("Type 'Increment' to get value and increment by 1")
 	fmt.Println("--------------------")
-
-	//Infinite loop to listen for clients input.
 	for {
-		fmt.Print("-> ")
-
-		//Read input into var input and any errors into err
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 		input = strings.TrimSpace(input) //Trim input
-
-		if !conReady(server) {
-			log.Printf("Client %s: something was wrong with the connection to the server :(", *clientsName)
-			continue
+		if input == "Increment" {
+			incrementVal()
 		}
-
-		//Convert string to int64, return error if the int is larger than 32bit or not a number
-		val, err := strconv.ParseInt(input, 10, 64)
-		if err != nil {
-			if input == "hi" {
-				sayHi()
-			}
-			continue
-		}
-		incrementVal(val)
 	}
 }
 
-func incrementVal(val int64) {
+func incrementVal() {
+	values := []int64{}
 	//create amount type
 	amount := &gRPC.Amount{
 		ClientName: *clientsName,
-		Value:      val, //cast from int to int32
+		Value:      1, //cast from int to int32
 	}
-
-	//Make gRPC call to server with amount, and recieve acknowlegdement back.
-	ack, err := server.Increment(context.Background(), amount)
-	if err != nil {
-		log.Printf("Client %s: no response from the server, attempting to reconnect", *clientsName)
-		log.Println(err)
+	log.Printf("Client called increment")
+	for i := 0; i < 3; i++ {
+		//Make gRPC call to server with amount, and recieve acknowlegdement back.
+		ack, err := servers[i].Increment(context.Background(), amount)
+		if err != nil || ack == nil {
+			log.Printf("Port: 500%d server has crashed\n", i)
+			fmt.Printf("Port: 500%d server has crashed\n", i)
+			continue
+		}
+		values = append(values, ack.NewValue)
 	}
-
-	// check if the server has handled the request correctly
-	if ack.NewValue >= val {
-		fmt.Printf("Success, the new value is now %d\n", ack.NewValue)
-	} else {
-		// something could be added here to handle the error
-		// but hopefully this will never be reached
-		fmt.Println("Oh no something went wrong :(")
-	}
-}
-
-func sayHi() {
-	// get a stream to the server
-	stream, err := server.SayHi(context.Background())
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// send some messages to the server
-	stream.Send(&gRPC.Greeding{ClientName: *clientsName, Message: "Hi"})
-	stream.Send(&gRPC.Greeding{ClientName: *clientsName, Message: "How are you?"})
-	stream.Send(&gRPC.Greeding{ClientName: *clientsName, Message: "I'm fine, thanks."})
-
-	// close the stream
-	farewell, err := stream.CloseAndRecv()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println("server says: ", farewell)
-}
-
-// Function which returns a true boolean if the connection to the server is ready, and false if it's not.
-func conReady(s gRPC.TemplateClient) bool {
-	return ServerConn.GetState().String() == "READY"
+	log.Printf("Returned increment value is %d\n", values[0])
+	fmt.Printf("Returned increment value is %d\n", values[0])
 }
 
 // sets the logger to use a log.txt file instead of the console
--- server/server.go
+++ server/server.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
+	"strconv"
 	"sync"
 
 	// this has to be the same as the go.mod module,
@@ -19,7 +19,6 @@ import (
 
 type Server struct {
 	gRPC.UnimplementedTemplateServer        // You need this line if you have a server
-	name                             string // Not required but useful if you want to name your server
 	port                             string // Not required but useful if your server needs to know what port it's listening to
 
 	incrementValue int64      // value that clients can increment.
@@ -28,13 +27,20 @@ type Server struct {
 
 // flags are used to get arguments from the terminal. Flags take a value, a default value and a description of the flag.
 // to use a flag then just add it as an argument when running the program.
-var serverName = flag.String("name", "default", "Senders name") // set with "-name <name>" in terminal
-var port = flag.String("port", "5400", "Server port")           // set with "-port <port>" in terminal
+var port *string        // set with "-port <port>" in terminal
+var serverName = "port" // name of the server
+var serverPort string   // port of the server port
+var serverSomething = "server port"
+var firstIncrement bool = true
 
 func main() {
-
-	// f := setLog() //uncomment this line to log to a log.txt file instead of the console
-	// defer f.Close()
+	arg1, _ := strconv.ParseInt(os.Args[1], 10, 64)
+	serverPort32 := int64(arg1) + 5000
+	serverPort = strconv.FormatInt(serverPort32, 10)
+	fmt.Println(serverPort)
+	port = flag.String(serverName, serverPort, serverSomething)
+	f := setLog() //uncomment this line to log to a log.txt file instead of the console
+	defer f.Close()
 
 	// This parses the flags and sets the correct/given corresponding values.
 	flag.Parse()
@@ -47,12 +53,12 @@ func main() {
 }
 
 func launchServer() {
-	log.Printf("Server %s: Attempts to create listener on port %s\n", *serverName, *port)
+	log.Printf("Attempts to create listener on port %s\n", *port)
 
 	// Create listener tcp on given port or default port 5400
 	list, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", *port))
 	if err != nil {
-		log.Printf("Server %s: Failed to listen on port %s: %v", *serverName, *port, err) //If it fails to listen on the port, run launchServer method again with the next value/port in ports array
+		log.Printf("Failed to listen on port %s: %v", *port, err) //If it fails to listen on the port, run launchServer method again with the next value/port in ports array
 		return
 	}
 
@@ -63,56 +69,18 @@ func launchServer() {
 
 	// makes a new server instance using the name and port from the flags.
 	server := &Server{
-		name:           *serverName,
 		port:           *port,
 		incrementValue: 0, // gives default value, but not sure if it is necessary
 	}
 
 	gRPC.RegisterTemplateServer(grpcServer, server) //Registers the server to the gRPC server.
 
-	log.Printf("Server %s: Listening at %v\n", *serverName, list.Addr())
+	log.Printf("Listening at %v\n", list.Addr())
 
 	if err := grpcServer.Serve(list); err != nil {
 		log.Fatalf("failed to serve %v", err)
 	}
 	// code here is unreachable because grpcServer.Serve occupies the current thread.
-}
-
-// The method format can be found in the pb.go file. If the format is wrong, the server type will give an error.
-func (s *Server) Increment(ctx context.Context, Amount *gRPC.Amount) (*gRPC.Ack, error) {
-	// locks the server ensuring no one else can increment the value at the same time.
-	// and unlocks the server when the method is done.
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
-	// increments the value by the amount given in the request,
-	// and returns the new value.
-	s.incrementValue += int64(Amount.GetValue())
-	return &gRPC.Ack{NewValue: s.incrementValue}, nil
-}
-
-func (s *Server) SayHi(msgStream gRPC.Template_SayHiServer) error {
-	for {
-		// get the next message from the stream
-		msg, err := msgStream.Recv()
-
-		// the stream is closed so we can exit the loop
-		if err == io.EOF {
-			break
-		}
-		// some other error
-		if err != nil {
-			return err
-		}
-		// log the message
-		log.Printf("Received message from %s: %s", msg.ClientName, msg.Message)
-	}
-
-	// be a nice server and say goodbye to the client :)
-	ack := &gRPC.Farewell{Message: "Goodbye"}
-	msgStream.SendAndClose(ack)
-
-	return nil
 }
 
 // Get preferred outbound ip of this machine
@@ -143,4 +111,20 @@ func setLog() *os.File {
 	}
 	log.SetOutput(f)
 	return f
+}
+
+func (s *Server) Increment(ctx context.Context, Amount *gRPC.Amount) (*gRPC.Ack, error) {
+	// locks the server ensuring no one else can increment the value at the same time.
+	// and unlocks the server when the method is done.
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	// increments the value by the amount given in the request,
+	// and returns the new value.
+	if firstIncrement == true {
+		firstIncrement = false
+		return &gRPC.Ack{NewValue: 0, Port: s.port}, nil
+	}
+	s.incrementValue += int64(Amount.GetValue())
+	return &gRPC.Ack{NewValue: s.incrementValue, Port: s.port}, nil
 }
